6787b5d81d457b40837c2a981a1da2c4a43ef828
Shiv Kumar <sivu10196@gmail.com>
2019-08-21 15:51:52 +0530

    Added New VC

---
+++ GitDemo/NEWViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NEWViewController.swift
+//  GitDemo
+//
+//  Created by Shiv kumar on 8/21/19.
+//  Copyright © 2019 Shiv kumar. All rights reserved.
+//
+
+import UIKit
+
+class NEWViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Changes made by shiv
         // Do any additional setup after loading the view.
     }
 
